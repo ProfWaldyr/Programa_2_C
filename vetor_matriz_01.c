577a02871ea330f1c5ebb47531de0f4eedca2700
Prof_Waldyr <102627015+ProfWaldyr@users.noreply.github.com>
2025-10-22 13:44:46 -0300

    .

--- vetor_matriz_01.c
+++ vetor_matriz_01.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
 
+int main() {
+
+int vetor [10] = {1,2,3,4,5,6,7,8,9,10};
+
+int soma = 0;
+
+// Calculamos a soma dos elementos do vetor;
+
+for (int = 0; i <=10; i ++); {
+    soma += vetor [i];
+
+    }
+
+    print("A soma dos elementos do vetor é: %d\n", soma);
+
+
+return 0
+
+}
