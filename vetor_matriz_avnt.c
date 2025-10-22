2b1fc348ba7ffc68e648eea632a40e451a7b256e
Prof_Waldyr <102627015+ProfWaldyr@users.noreply.github.com>
2025-10-22 13:35:43 -0300

    .

--- vetor_matriz_avnt.c
+++ vetor_matriz_avnt.c
@@ -5,4 +5,18 @@ int main() {
     // Declaração matriz 3x3 inteiros;
 
     // inicialização de estrutura de repetição "FOR" aninhados, matriz 3x3;
-    
+
+    for (int i = 0; i < 3; i ++) {
+        
+        for (int j = 0; j < 3; j ++)  {
+
+            matriz [i] [j] = i + J; // atribuindo valores a matriz
+            printf("matriz [%d][%d] = %d\n", i , j, matriz [i] [j]);
+
+
+        }
+
+    }
+
+return 0;
+    }
